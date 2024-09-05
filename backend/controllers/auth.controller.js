8eb7fcb828c63938429479c674151a3da54d6f59
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2024-09-05 22:23:23 +0530

    trying to fix cookies issue

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -130,7 +130,7 @@ export async function login(req, res) {
         .json({ success: false, message: "Invalid Password" });
     }
 
-    genTokenAndSendCookie(user.email, res);
+    genTokenAndSendCookie(user._id, res);
 
     res.status(200).json({
       success: true,
