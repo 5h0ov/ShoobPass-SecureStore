a9c27c0686d635d959a04b2b057eb26f6d5d3c31
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2024-09-06 12:02:50 +0530

    fixing getAuth bug

--- frontend/src/redux/auth/authSlice.js
+++ frontend/src/redux/auth/authSlice.js
@@ -64,7 +64,12 @@ export const logout = createAsyncThunk('auth/logout', async (_, { rejectWithValu
 
 export const getAuth = createAsyncThunk('auth/getAuth', async (_, { rejectWithValue }) => {
   try {
-    const res = await axios.get(`${API_URL}/api/auth/getAuth`);
+    const token = localStorage.getItem('jwt-shoobpass');
+    const res = await axios.get(`${API_URL}/auth/getAuth`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     console.log("user:",res.data.user);
     return res.data.user;
   } catch (error) {
