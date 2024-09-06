8e47b9bac99c4a4d11af2f8722abec8d802d96d2
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2024-09-06 12:18:57 +0530

    trying to fix getAuth

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -11,14 +11,14 @@ export const genTokenAndSendCookie = (userId, res) => {
     // so when you try to access it, you will get undefined
     // and you will spend hours debugging it
     
-    res.cookie("jwt-shoobpass", token, {
-        maxAge: 8*24*60*60*1000,   // 8 days in milliseconds
-        httpOnly: true , // cookie is only accessible by the server, and prevent XSS attacks cross-site scripting attacks and make it not accessible via JS
-        // sameSite: "strict",  // cookie is not sent with cross-origin requests, protecting from forgery attacks
-        sameSite: "none",  // cookie is sent with cross-origin requests, protecting from forgery attacks
-        secure: true,// cookie is only sent over HTTPS, as in dev http is false and in production it is true
-        domain: ENV_VARS.NODE_ENV === "development" ? "localhost" : "shoob-pass-secure-store-wrqx.vercel.app", // domain of the cookie
-    });
+    // res.cookie("jwt-shoobpass", token, {
+    //     maxAge: 8*24*60*60*1000,   // 8 days in milliseconds
+    //     httpOnly: true , // cookie is only accessible by the server, and prevent XSS attacks cross-site scripting attacks and make it not accessible via JS
+    //     // sameSite: "strict",  // cookie is not sent with cross-origin requests, protecting from forgery attacks
+    //     sameSite: "none",  // cookie is sent with cross-origin requests, protecting from forgery attacks
+    //     secure: true,// cookie is only sent over HTTPS, as in dev http is false and in production it is true
+    //     domain: ENV_VARS.NODE_ENV === "development" ? "localhost" : "shoob-pass-secure-store-wrqx.vercel.app", // domain of the cookie
+    // });
     
     return token;
 };
