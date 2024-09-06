2a66d153819443050b3a6f408be8e186c6de50d9
Shuvadipta Das <shuvadiptadas8820@gmail.com>
2024-09-06 11:27:32 +0530

    testing fix

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -16,7 +16,7 @@ export const genTokenAndSendCookie = (userId, res) => {
         httpOnly: true , // cookie is only accessible by the server, and prevent XSS attacks cross-site scripting attacks and make it not accessible via JS
         // sameSite: "strict",  // cookie is not sent with cross-origin requests, protecting from forgery attacks
         sameSite: "none",  // cookie is sent with cross-origin requests, protecting from forgery attacks
-        secure: ENV_VARS.NODE_ENV !== "development",// cookie is only sent over HTTPS, as in dev http is false and in production it is true
+        secure: true,// cookie is only sent over HTTPS, as in dev http is false and in production it is true
         domain: ENV_VARS.NODE_ENV === "development" ? "localhost" : "shoob-pass-secure-store-wrqx.vercel.app", // domain of the cookie
     });
     
